633b425913af6b60a2ed21556f4a403684c8af70
Pradeep <pradeep.pyro@gmail.com>
2017-04-28 17:00:40 +0800

    Add simple contiguous 2d array class for internal use

---
+++ openspline/array2.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+using std::cout;
+using std::endl;
+namespace ospl {
+
+/**
+A simple class for representing 2D dynamic arrays.
+This would most likely be replaced with std::dynarray or local runtime-arrays
+in future.
+*/
+template <typename T>
+class array2 {
+public:
+	array2(size_t nRows, size_t nCols, T fillValue = 0.0)
+		: rows(nRows), cols(nCols) {
+		data.resize(rows * cols, fillValue);
+	}
+	T operator()(size_t row, size_t col) const {
+		return data[row*cols + col];
+	}
+	T& operator()(size_t row, size_t col) {
+		return data[row*cols + col];
+	}
+private:
+	size_t rows, cols;
+	std::vector<T> data;
+};
+
+}
