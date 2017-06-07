21864ae59ccbee22678e0bf7e1f5cc912948c6ba
Pradeep <pradeep.pyro@gmail.com>
2017-06-07 16:42:31 +0800

    Better naming and return types

--- openspline/nurbs_curve.h
+++ openspline/nurbs_curve.h
@@ -21,126 +21,105 @@ class NurbsCurve {
 public:
 	typedef glm::vec<nd, T> vecnt;
 
-	NurbsCurve() {};
-
 	NurbsCurve(unsigned int degree, const std::vector<double> &knots,
 		const std::vector<vecnt> &controlPoints) {
 		if (!isValidRelation(degree, knots.size(), 
 			controlPoints.size())) {
 			throw std::logic_error("nKnots != degree + nCtrlPts + 1");
 		}
-		deg_ = degree;
-		knots_ = knots;
-		cp_ = controlPoints;
-		isRat_ = false;
-		isClamp0_ = false;
-		isClamp1_ = false;
+		this->deg = degree;
+		this->knots = knots;
+		this->cp = controlPoints;
+		this->w.resize(controlPoints.size(), 1.0);
+		this->isRat = false;
 	}
 
 	NurbsCurve(unsigned int degree, const std::vector<double> &knots, 
 		const std::vector<vecnt> &controlPoints, const std::vector<T> &weights) {
 		NurbsCurve(degree, knots, controlPoints);
-		w_ = weights;
-		isRat_ = true;
+		if (!isValidRelation(degree, knots.size(),
+			controlPoints.size())) {
+			throw std::logic_error("nKnots != degree + nCtrlPts + 1");
+		}
+		this->deg = degree;
+		this->knots = knots;
+		this->cp = controlPoints;
+		this->w = weights;
+		this->isRat = true;
 	}
-	
+
+	int degree() const {
+		return deg;
+	}
+
 	vecnt point(double u) {
 		vecnt pt;
-		if (!isRat_) {
-			nurbsCurvePoint<nd, T>(u, deg_, knots_, cp_, pt);
+		if (!rational()) {
+			nurbsCurvePoint<nd, T>(u, deg, knots, cp, pt);
 		}
 		else {
-			nurbsRationalCurvePoint<nd, T>(u, deg_, knots_, cp_, w_, pt);
+			nurbsRationalCurvePoint<nd, T>(u, deg, knots, cp, w, pt);
 		}
 		return pt;
 	}
 
-	void pointAndDerivatives(double u, int nDers, std::vector<vecnt> &ptder) {
-		if (!isRat_) {
-			nurbsCurveDerivatives<nd, T>(u, deg_, knots_, cp_, nDers, ptder);
+	void derivatives(double u, int nDers, std::vector<vecnt> &ptder) {
+		if (!rational()) {
+			nurbsCurveDerivatives<nd, T>(u, deg, knots, cp, nDers, ptder);
 		}
 		else {
-			//nurbsRationalCurveDerivatives<nd, T>(u, deg_, knots_, cp_, w_, 1, ptder);
+			nurbsRationalCurveDerivatives<nd, T>(u, deg, knots, cp, w, 1, ptder);
 		}
 	}
 
-	vecnt tangent(double u) {
+	vecnt tangent(double u, bool normalize = true) {
 		std::vector<vecnt> ders;
-		pointAndDerivatives(u, 1, ders);
-		return glm::normalize(ders[1]);
+		derivatives(u, 1, ders);
+		return normalize ? glm::normalize(ders[1]) : ders[1];
 	}
 
 	vecnt controlPoint(int i) const {
-		return cp_[i];
+		return cp[i];
 	}
 
 	void setControlPoint(int i, vecnt pt) {
-		cp_[i] = pt;
+		cp[i] = pt;
 	}
 
-	std::vector<vecnt> controlPoints() const {
-		return cp_;
+	const std::vector<vecnt>& controlPoints() const {
+		return cp;
 	}
 
-	int numControlPoints() const {
-		return cp_.size();
+	size_t numControlPoints() const {
+		return cp.size();
+	}
+
+	T weight(int i) const {
+		return w[i];
+	}
+
+	void setWeight(int i, T weight) {
+		w[i] = weight;
+	}
+
+	const std::vector<double>& knotVector() const {
+		return knots;
+	}
+
+	bool rational() const {
+		return isRat;
 	}
 
 	void setRational(bool isRational) {
-		isRat_ = isRational;
+		isRat = isRational;
 	}
-
-	void clampStart() {
-		if (isClamp0_) {
-			return;
-		}
-		double startKnot = knots_[0];
-		knots_.insert(knots_.begin(), deg_, startKnot);
-		isClamp0_ = true;
-	}
-
-	void unclampStart() {
-		if (!isClamp0_) {
-			return;
-		}
-		knots_.erase(knots_.begin(), knots_.begin() + deg_);
-		isClamp0_ = false;
-	}
-
-	void clampEnd() {
-		if (isClamp1_) {
-			return;
-		}
-		double endKnot = knots_[knots_.size() - 1];
-		knots_.insert(knots_.end(), deg_, endKnot);
-		isClamp1_ = true;
-	}
-
-	void unclampEnd() {
-		if (!isClamp1_) {
-			return;
-		}
-		knots_.erase(knots_.end() - deg_, knots_.end());
-		isClamp1_ = false;
-	}
-
-	void setClosed(bool flag) {
-		if (flag && !isClosed_) {
-			// wrap knots and duplicate last controlpoint
-		}
-		if (!flag && isClosed_) {
-			// remove duplicate control point and unwrap knots
-		}
-	}
-
+	
 private:
-	unsigned int deg_;
-	std::vector<double> knots_;
-	std::vector<vecnt> cp_;
-	std::vector<T> w_;
-	bool isRat_;
-	bool isClamp0_, isClamp1_;
-	bool isClosed_;
+	unsigned int deg;
+	std::vector<double> knots;
+	std::vector<vecnt> cp;
+	std::vector<T> w;
+	bool isRat;
 };
 
 // Typedefs for ease of use
