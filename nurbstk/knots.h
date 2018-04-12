cc46660065cec9f1b51ed0eea60b99085fdbc9c7
Pradeep <pradeep.pyro@gmail.com>
2018-04-12 23:52:21 +0800

    Add knot insertion

--- nurbstk/knots.h
+++ nurbstk/knots.h
@@ -10,6 +10,7 @@ the LICENSE.txt file.
 
 #pragma once
 
+#include <algorithm>
 #include <vector>
 
 template <typename T>
@@ -103,4 +104,34 @@ bool isKnotVectorClosed(unsigned int degree, const std::vector<T> &knots) {
         }
     }
     return true;
+}
+
+template <typename T, int n>
+void curveInsertKnot(T u, unsigned int deg, std::vector<glm::vec<T, n> &knots, std::vector<glm::vec<T, n> &cp) {
+    int span = findSpan(u, deg, knots);
+    // Insert new control point and knot between span and (span + 1)
+    knots.insert(span, u);
+    cp.insert(span, tvecn(0.0));
+    // Compute new positions for affected control points
+    for (int i = span - deg + 1; i <= span; ++i) {
+        T a = (u - knots[i]) / (knots[i + deg] - knots[i]);
+        cp[i] = (1 - a) * cp[i - 1] + a * cp[i];
+    }
+}
+
+template <typename T, int n>
+void surfaceInsertKnotU(T u, unsigned int deg, std::vector<glm::vec<T, n> &knots, std::vector<glm::vec<T, n> &cp) {
+    int span = findSpan(u, deg, knots);
+    // Insert new control point and knot between span and (span + 1)
+    knots.insert(span, u);
+    for (int i = 0; i < cp.size(); ++i) {
+        cp[i].insert(span, tvecn(0.0));
+    }
+    // Compute new positions for affected control points
+    for (int i = 0; i < cp.size(); ++i) {
+        for (int j = span - deg + 1; j <= span; ++j) {
+            T a = (u - knots[i]) / (knots[i + deg] - knots[j]);
+            cp[i][j] = (1 - a) * cp[i][j - 1] + a * cp[i][j];
+        }
+    }
 }
