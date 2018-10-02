2a1c4612e6de5784fe2c3bc553e2156b37ab6084
Pradeep <pradeep.pyro@gmail.com>
2018-10-02 22:40:22 +0800

    Add header for importing entire library

---
+++ tinynurbs/tinynurbs.h
@@ -0,0 +1,15 @@
+/**
+@file
+@brief Import the entire library
+
+Use of this source code is governed by a BSD-style license that can be found in
+the LICENSE file.
+ */
+
+#include "core/basis.h"
+#include "core/evaluate.h"
+#include "core/check.h"
+#include "core/modify.h"
+#include "geometry/curve.h"
+#include "geometry/surface.h"
+#include "io/obj.h"
